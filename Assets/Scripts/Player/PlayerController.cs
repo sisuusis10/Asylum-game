a6aa102cda09caef232b048e4d90158184e222a5
sisuusis10 <johanknielsen@gmail.com>
2022-11-08 21:41:39 +0100

    LET THERE BE SOUND!
    walking now makes sound effects play

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,12 +8,16 @@ public class PlayerController : MonoBehaviour {
     public static PlayerController p;
     public bool IsLocked = true;
 
+    //Components
     private Camera Cam;
+    private Animator Anim;
     private Rigidbody _rb;
     private CharacterController CharController;
+    private AudioSource _Audio;
     //Movement
     private Vector3 MovementVector;
     private float SpeedMultiplier = 10f, _speed;
+    public bool IsRunning = false;
 
     //Camera
     private Vector3 RotVector, FinalRotVector, ShakeDirection;
@@ -26,7 +30,8 @@ public class PlayerController : MonoBehaviour {
         Cam = Camera.main;
         _rb = this.GetComponent<Rigidbody>();
         CharController = this.GetComponent<CharacterController>();
-        
+        Anim = this.GetComponent<Animator>();
+        _Audio = this.GetComponent<AudioSource>();
         //Unlock
         IsLocked = false;
     }
@@ -35,11 +40,14 @@ public class PlayerController : MonoBehaviour {
     public void Update() {
         if(!IsLocked) {
             Mov();
+            WalkAnimSfx();
         }
     }
 
     public void Mov() {
-        SpeedMultiplier = (!Input.GetKey(KeyCode.LeftShift)) ? 5f : 15f;
+        IsRunning = (!Input.GetKey(KeyCode.LeftShift)) ? false : true;
+        SpeedMultiplier = (!IsRunning) ? 5f : 15f;
+
         _speed = Mathf.Lerp(_speed, SpeedMultiplier, 0.05f);
         //Movement
         MovementVector.x = Input.GetAxisRaw("Horizontal");
@@ -55,7 +63,7 @@ public class PlayerController : MonoBehaviour {
         //Apply
         Vector3 dir = this.transform.TransformDirection(MovementVector.normalized);
         CharController.Move((dir) * _speed * Time.deltaTime);
-    
+        
     }
     private void LateUpdate() {
         if(!IsLocked) {
@@ -67,4 +75,43 @@ public class PlayerController : MonoBehaviour {
             Cam.transform.rotation = Quaternion.Euler(FinalRotVector + ShakeDirection);
         }
     }
+
+
+    //Walk SFX & anim variables
+    private float timer, timer_max;
+    private AudioClip stepsfx;
+    public void WalkAnimSfx() {
+        if(MovementVector != Vector3.zero) {
+            timer += Time.deltaTime;
+            Anim.SetBool("IsWalking", true);
+            if (!IsRunning) {
+                timer_max = 0.5f;
+            }
+            else {
+                timer_max = 0.2f;
+            }
+
+            if(timer >= timer_max) {
+                timer = 0;
+
+                RaycastHit hit;
+                if(Physics.Raycast(this.transform.position, Vector3.down, out hit)) {
+                    SoundMaterial s = hit.collider.gameObject.GetComponent<SoundMaterial>();
+                    if (s != null) {
+                        _Audio.clip = SoundSource.s.Get_StepSound(s.Material);
+                    }
+                }
+
+                _Audio.pitch = Random.Range(0.9f, 1.1f);
+
+                _Audio.Play();
+
+            }
+
+        } else {
+            Anim.SetBool("IsWalking", false);
+        }
+
+    }
+
 }
